8f45c87a753cfa5a7d7e33f8da520b68b2f577e8
Vitaly Galperin <vitalyglll@gmail.com>
2020-10-23 17:53:12 +0300

    Flask. HomeWork №1 is ready for verification

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,34 +1,57 @@
 import datetime
+import random
+import re
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/hello_word')
-def test_function():
-   pass
+def hello_function():
+    return "Привет, мир!"
 
 
 @app.route('/cars')
-def test_function():
-   pass
+def cars_function():
+    return "Chevrolet, Renault, Ford, Lada"
 
 
 @app.route('/cats')
-def test_function():
-   pass
+def cats_function():
+    cats = ['Корниш рекс', 'Русская голубая', 'Шотландская вислоухая', 'Мэйн-Кун', 'Манчкин']
+    return random.choice(cats)
 
 
 @app.route('/get_time/now')
-def test_function():
-   pass
+def now_function():
+    current_time = datetime.datetime.now()
+    return f"Точное время {current_time}"
 
 
 @app.route('/get_time/future')
-def test_function():
-   pass
+def future_function():
+    future_time = datetime.datetime.now() + datetime.timedelta(hours=1)
+    return f"Точное время через час будет {future_time}"
 
 
 @app.route('/get_random_word')
-def test_function():
-   pass
+def word_function():
+
+    while True:
+        with open('war_and_peace.txt', 'r', encoding='cp1251') as file:
+            random_line = random.choice(file.readlines())
+            words = random_line.split(' ')
+            if len(words) > 0:
+                word = random.choice(words)
+                format_word = re.sub(r"[,.!?;:-]", "", word)
+                if format_word and len(format_word) > 1:
+                    break
+    return format_word
+
+
+@app.route('/counter')
+def count_func():
+    if not hasattr(count_func, 'count'):
+        count_func.count = 0
+    count_func.count += 1
+    return 'Страница открывалась/обновлялась ' + str(count_func.count) + ' раз(а)'
--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -3,8 +3,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/test')
 def test_function():
-   return 'Это тестовая страничка, ответ сгенерирован в %s' % \
-                     datetime.datetime.now().utcnow()
+    return 'Это тестовая страничка, ответ сгенерирован в %s' % \
+           datetime.datetime.now().utcnow()
 
+
+@app.route('/hello/world')
+def hello_func():
+    return "Hello, world!"
+
+
+@app.route('/counter')
+def count_func():
+    if not hasattr(count_func, 'count'):
+        count_func.count = 0
+    count_func.count += 1
+    return 'Страница открывалась/обновлялась ' + str(count_func.count) + ' раз(а)'
